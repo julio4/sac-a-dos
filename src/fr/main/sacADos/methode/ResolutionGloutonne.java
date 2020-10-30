f5e4697488f6e8c573b08187c6817a0e0153fb8b
julio4 <jules.doumeche@gmail.com>
2020-10-30 10:33:04 +0100

    QuickSort

--- src/fr/main/sacADos/methode/ResolutionGloutonne.java
+++ src/fr/main/sacADos/methode/ResolutionGloutonne.java
@@ -30,12 +30,52 @@ public class ResolutionGloutonne extends Resolution {
         //Clone objPossible
         ArrayList<Objet> tri = new ArrayList<>(objets);
         //Tri par rapport
-        tri.sort(Objet.parRapport());
+        //tri.sort(Objet.parRapport());
+        triRapideRec(tri, 0, tri.size() - 1);
+
         //Ajout dans le sac
         Iterator<Objet> itTri = tri.iterator();
         while(itTri.hasNext() && sac.getPoids() < sac.getPoidsMax()) {
             sac.ajouter(itTri.next());
         }
         //Sac trié!
+    }
+
+    /**
+     * Implémentation de l'algorithme de tri rapide Quicksort par récursivité
+     * pour trier une liste d'Objets par rapport (décroissant)
+     *
+     * @param objets la liste d'objets à trier
+     * @param premier l'indice minimal
+     * @param dernier l'indice maximal
+     */
+    private void triRapideRec(List<Objet> objets, int premier, int dernier) {
+        if(premier < dernier) {
+            int pivot = (dernier - premier) / 2 + premier;
+            pivot = repartir(objets, premier, dernier, pivot);
+            triRapideRec(objets, premier, pivot - 1);
+            triRapideRec(objets, pivot + 1, dernier);
+        }
+    }
+
+    /**
+     * Permet de répartir les objets autour du pivot
+     *
+     * @param objets la liste d'objets à répartir
+     * @param premier l'indice minimal
+     * @param dernier l'indice maximal
+     * @param pivot l'indice du pivot
+     */
+    private int repartir(List<Objet> objets, int premier, int dernier, int pivot) {
+        Collections.swap(objets, pivot, dernier);
+        int i = premier;
+        for (int j = premier; j < dernier; ++j) {
+            if(objets.get(j).getRapport() >= objets.get(dernier).getRapport()) {
+                Collections.swap(objets, j, i);
+                i++;
+            }
+        }
+        Collections.swap(objets, dernier, i);
+        return i;
     }
 }
